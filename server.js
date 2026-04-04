5ffbf71d5dd850ccd7eddff8d4d37a7a50b56b49
Mistickalone <126264682+Mistickalone@users.noreply.github.com>
2026-04-04 11:16:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const http = require('http');
-const https = require('https');
-
-const USERNAME = "848278428260";
-const PASSWORD = "232140880725";
-const PROVIDER_URL = "http://freeiptv.ottc.xyz:80/get.php?username=" + USERNAME + "&password=" + PASSWORD + "&type=m3u_plus&output=ts";
 
 http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'application/x-mpegurl');
-  res.setHeader('Transfer-Encoding', 'chunked');
-  http.get(PROVIDER_URL, (providerRes) => {
-    providerRes.pipe(res);
-  });
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('#EXTM3U\n#EXTINF:-1,Test\nhttp://test.com/stream.ts');
 }).listen(process.env.PORT || 3000);
